b7b5eef1258658ef3532bdf4c112c8cd97f562e5
HQuang0910 <134147330+ItsAzura@users.noreply.github.com>
2024-06-06 10:09:42 +0700

    feat: Add API route for adding a new blog

---
+++ src/app/api/addblog/route.js
@@ -0,0 +1,53 @@
+import connectToDb from '@/database/db';
+import Blog from '@/models/blog';
+import { NextResponse } from 'next/server';
+
+//Tạo schema để validate dữ liệu
+const AddNewBlog = Joi.object({
+  title: Joi.string().required(),
+  content: Joi.string().required(),
+});
+
+//Hàm này sẽ được gọi khi có request POST tới /api/addblog
+export const POST = async (req, res) => {
+  try {
+    //Kết nối tới database
+    await connectToDb();
+
+    //Lấy dữ liệu từ request
+    const extractBlogData = await req.json();
+    const { title, content } = extractBlogData;
+
+    //Kiểm tra dữ liệu
+    const { error } = AddNewBlog.validate({ title, content });
+
+    if (error) {
+      return NextResponse.json(
+        { message: error.details[0].message },
+        { status: 400 }
+      );
+    }
+
+    //Tạo mới một bài viết
+    const newBlog = await Blog.create(extractBlogData);
+
+    //Kiểm tra xem tạo mới thành công không?
+    if (newBlog) {
+      return NextResponse.json(
+        { message: 'Blog added successfully' },
+        { status: 201 }
+      );
+    } else {
+      return NextResponse.json(
+        { message: 'Failed to add blog' },
+        { status: 400 }
+      );
+    }
+  } catch (error) {
+    console.log(error);
+    return NextResponse.json(
+      { message: 'Internal server error' },
+      { status: 500 }
+    );
+  }
+};
