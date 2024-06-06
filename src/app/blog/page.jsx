7e32750822634f7d22a26bce52c8b399791d909c
HQuang0910 <134147330+ItsAzura@users.noreply.github.com>
2024-06-06 16:26:51 +0700

    chore: Update blog page with dynamic blog list and styling

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -1,8 +1,50 @@
-import React from 'react';
+'use client';
+import React, { useEffect } from 'react';
 import BlogOverView from '../components/blogOverView/BlogOverView';
+import Loading from '../components/loading/Loading';
+import { useState } from 'react';
+import Link from 'next/link';
+import styles from './blog.module.css';
 
 const blogPage = () => {
-  return <BlogOverView />;
+  const [blogs, setBlogs] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  const handleFetchBlogs = async () => {
+    try {
+      const res = await fetch('/api/getblogs');
+      const data = await res.json();
+      if (data.data) {
+        setBlogs(data.data);
+        setLoading(false);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    handleFetchBlogs();
+  }, []);
+
+  if (loading) return <Loading />;
+
+  return (
+    <div className={styles.mainContainer}>
+      <div className={styles.groupTitle}>
+        <h1>All Blogs</h1>
+        <BlogOverView />
+      </div>
+
+      <div className={styles.container}>
+        {blogs.map((blog) => (
+          <Link href={`/blog/${blog._id}`} key={blog._id}>
+            <h3>{blog.title}</h3>
+          </Link>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default blogPage;
