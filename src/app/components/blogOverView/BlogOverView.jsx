bd62ec8a1b65fbb1097c0f8b7fead315a1e94c8f
HQuang0910 <134147330+ItsAzura@users.noreply.github.com>
2024-06-06 16:26:13 +0700

    chore: Update blog overview page with add blog button styling

--- src/app/components/blogOverView/BlogOverView.jsx
+++ src/app/components/blogOverView/BlogOverView.jsx
@@ -5,10 +5,19 @@ import Link from 'next/link';
 
 const BlogOverView = () => {
   return (
-    <div className={styles.container}>
-      <div className="">
-        <Link href="/blog/addblog">Add Blog</Link>
-      </div>
+    <div className={styles.btnAdd}>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+      >
+        <path
+          fill="currentColor"
+          d="M19 11h-6V5a1 1 0 0 0-2 0v6H5a1 1 0 0 0 0 2h6v6a1 1 0 0 0 2 0v-6h6a1 1 0 0 0 0-2"
+        />
+      </svg>
+      <Link href="/blog/addblog">Add Blog</Link>
     </div>
   );
 };
